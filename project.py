8364d513f1040a41e8abad5cf35e31b6e364b263
Michael Eliosov <eliosov.pn@ucu.edu.ua>
2022-12-12 22:35:58 +0200

    Improved readability

--- project.py
+++ project.py
@@ -1,3 +1,6 @@
+'''
+Contains an implementation of the A* algorithm and a few helper functions
+'''
 import math
 testing_graph = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
 testing_step = int(math.sqrt(25))
@@ -5,92 +8,112 @@ A = [0, 2]
 B = [3, 0]
 
 
+def heuristic(grid, goal, vertex):
+    '''
+    A heuristic function using absolute distance difference
+    '''
+    return abs(vertex[0] - goal[0]) + abs(vertex[1] - goal[1]) \
+        + abs(grid[vertex[0]][vertex[1]] - grid[goal[0]][goal[1]])
+
+
+def get_neighbors(grid, vertex):
+    '''
+    A function that gets all neighbors of a vertex
+    '''
+    neighbors = []
+    if vertex[0] > 0:
+        neighbors.append(
+            [(vertex[0]-1, vertex[1]),
+                grid[vertex[0]-1][vertex[1]]])
+    if vertex[1] > 0:
+        neighbors.append(
+            [(vertex[0], vertex[1]-1),
+                grid[vertex[0]][vertex[1]-1]])
+    if vertex[0] < len(grid)-1:
+        neighbors.append(
+            [(vertex[0]+1, vertex[1]),
+                grid[vertex[0]+1][vertex[1]]])
+    if vertex[1] < len(grid[0])-1:
+        neighbors.append(
+            [(vertex[0], vertex[1]+1),
+                grid[vertex[0]][vertex[1]+1]])
+    return neighbors
+
+
 def a_star_algorithm(grid, step, start, goal):
-    # open_list is a list of nodes which have been visited, but who's neighbors
-    # haven't all been inspected, starts off with the start node
-    # closed_list is a list of nodes which have been visited
+    '''
+    Uses the A* algorithm
+    '''
+    # open_list is a list of vertices which have been visited, but who's neighbors
+    # haven't all been inspected, starts with the start vertex
+    # closed_list is a list of vertices which have been visited
     # and who's neighbors have been inspected
-    def heuristic(vertex):
-        return abs(vertex[0] - goal[0]) + abs(vertex[1] - goal[1]) \
-            + abs(grid[vertex[0]][vertex[1]] - grid[goal[0]][goal[1]])
+
     start = tuple(start)
     goal = tuple(goal)
     open_list = set([start])
     closed_list = set([])
 
-    # g contains current distances from start_node to all other nodes
-    # the default value (if it's not found in the map) is +infinity
+    # distance_to_start contains currently known minimum distances from start to other vertices
     distance_to_start = {}
 
     distance_to_start[start] = 0
 
-    # parents contains an adjacency map of all nodes
+    # parents is an (incomplete) vector of vertices
     parents = {}
     parents[start] = start
 
     while len(open_list) > 0:
-        vertex = None
+        chosen_candidate = None
 
-        # find a node with the lowest value of f() - evaluation function
+        # find a vertex with the lowest value of heuristic() - evaluation function
         for candidate in open_list:
-            if not vertex or distance_to_start[candidate] \
-                    + heuristic(candidate) < distance_to_start[vertex] + heuristic(vertex):
-                vertex = candidate
+            if not chosen_candidate or distance_to_start[candidate] \
+                + heuristic(grid, goal, candidate) < distance_to_start[chosen_candidate] +\
+                    heuristic(grid, goal, chosen_candidate):
+                chosen_candidate = candidate
 
-        if vertex == goal:
+        if chosen_candidate == goal:
             reconst_path = []
-
-            while parents[vertex] != vertex:
-                reconst_path.append(vertex)
-                vertex = parents[vertex]
-
+            while parents[chosen_candidate] != chosen_candidate:
+                reconst_path.append(chosen_candidate)
+                chosen_candidate = parents[chosen_candidate]
             reconst_path.append(start)
             reconst_path.reverse()
             return reconst_path
 
-        candidates = []
-        if vertex[0] > 0:
-            candidates.append(
-                [(vertex[0]-1, vertex[1]), grid[vertex[0]-1][vertex[1]]])
-        if vertex[1] > 0:
-            candidates.append(
-                [(vertex[0], vertex[1]-1), grid[vertex[0]][vertex[1]-1]])
-        if vertex[0] < len(grid)-1:
-            candidates.append(
-                [(vertex[0]+1, vertex[1]), grid[vertex[0]+1][vertex[1]]])
-        if vertex[1] < len(grid[0])-1:
-            candidates.append(
-                [(vertex[0], vertex[1]+1), grid[vertex[0]][vertex[1]+1]])
-        # for all neighbors of the current node do
-        for (candidate, height) in candidates:
-            # if the current node isn't in both open_list and closed_list
-            # add it to open_list and note n as it's parent
-            if candidate not in open_list and candidate not in closed_list:
-                open_list.add(candidate)
-                parents[candidate] = vertex
-                distance_to_start[candidate] = distance_to_start[vertex] + \
-                    math.sqrt(step**2+(height-grid[vertex[0]][vertex[1]])**2)
+        # a list of [point, height]
+        neighbors = get_neighbors(grid, chosen_candidate)
+        for (neighbor, height) in neighbors:
+            # if a neighbor isn't in open_list and closed_list
+            # add it to open_list and note chosen_candidate as its parent
+            if neighbor not in open_list and neighbor not in closed_list:
+                open_list.add(neighbor)
+                parents[neighbor] = chosen_candidate
+                distance_to_start[neighbor] = distance_to_start[chosen_candidate] + \
+                    math.sqrt(
+                        step**2+(height-grid[chosen_candidate[0]][chosen_candidate[1]])**2)
 
-            # otherwise, check if it's quicker to first visit n, then m
-            # and if it is, update parent data and g data
-            # and if the node was in the closed_list, move it to open_list
+            # otherwise, check if it's quicker to first visit chosen_candidate, then neighbor
+            # and if it is, update parents and distance_to_start
+            # and if the node was in the closed_]list, move it to open_list
             else:
-                if distance_to_start[candidate] > distance_to_start[vertex] \
-                        + math.sqrt(step**2+(height-grid[vertex[0]][vertex[1]])**2):
-                    distance_to_start[candidate] = distance_to_start[vertex] + \
+                if distance_to_start[neighbor] > distance_to_start[chosen_candidate] \
+                        + math.sqrt(step**2 +
+                                    (height-grid[chosen_candidate[0]][chosen_candidate[1]])**2):
+                    distance_to_start[neighbor] = distance_to_start[chosen_candidate] + \
                         math.sqrt(
-                            step**2+(height-grid[vertex[0]][vertex[1]])**2)
-                    parents[candidate] = vertex
+                            step**2+(height-grid[chosen_candidate[0]][chosen_candidate[1]])**2)
+                    parents[neighbor] = chosen_candidate
 
-                    if candidate in closed_list:
-                        closed_list.remove(candidate)
-                        open_list.add(candidate)
+                    if neighbor in closed_list:
+                        closed_list.remove(neighbor)
+                        open_list.add(neighbor)
 
-        # remove n from the open_list, and add it to closed_list
-        # because all of his neighbors were inspected
-        open_list.remove(vertex)
-        closed_list.add(vertex)
-
+        # remove chosen_candidate from the open_list, and add it to closed_list
+        # because all of its neighbors were inspected
+        open_list.remove(chosen_candidate)
+        closed_list.add(chosen_candidate)
     print('Path does not exist!')
     return None
 
